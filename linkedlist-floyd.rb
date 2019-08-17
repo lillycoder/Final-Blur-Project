4e517230bfa8d26e35b664746bba2e85fee9057f
lillycoder <ltheinh@gmail.com>
2019-08-17 22:47:37 +0000

    linkedlist2.rb is the response to the first part of the Linked List #2 assignment for TA review

---
+++ linkedlist-floyd.rb
@@ -0,0 +1,79 @@
+#linked list1 exercise
+#Anne von Luhrte
+#GW Bootcamp
+#with great help from website:  addstar34.github.io/ruby-linked-list-reverse-using-mutation/
+#did not use this website until after I struggled for HOURS and HOURS on this project
+
+class LinkedListNode #linked node
+
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+#build stack
+
+class Stack
+
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+  def push(newvalue)
+    @data = LinkedListNode.new(newvalue, @data)
+  end
+
+  def pop
+    return print "nil/n" if @data.nil?
+    print "#{@data.value}\n"
+    @data = @data.next_node
+  end
+end
+
+def print_values(list)
+  if list
+    print "#{list.value} -->"
+    print_values(list.next_node)
+  else
+    print "nil/n"
+    puts " "
+    return
+  end
+end
+   
+def reverse_list(list, previous=nil) 
+  if list
+    next_node = list.next_node
+    list.next_node = previous
+    reverse_list(next_node, list)
+  end
+end
+
+# Main Program
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+puts "--------------"
+reverse_list(node3)
+print_values(node1)
+
+
+
+
+
+
+
+
+  
+
+
+
+
