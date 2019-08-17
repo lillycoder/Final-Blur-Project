08b638e264ff509f003b71984624245cde0a1a22
lillycoder <ltheinh@gmail.com>
2019-08-17 22:59:40 +0000

    submission of final Linked List project for TA review

--- linkedlist-floyd.rb
+++ linkedlist-floyd.rb
@@ -1,8 +1,11 @@
 #linked list1 exercise
 #Anne von Luhrte
 #GW Bootcamp
-#with great help from website:  addstar34.github.io/ruby-linked-list-reverse-using-mutation/
+#with great help from website:  addstar34.github.io/ruby-linked-list-floyd-cycle-detection/
 #did not use this website until after I struggled for HOURS and HOURS on this project
+#let me be clear I DO NOT UNDERSTAND this!!!!  I will talk to my mentor but I think I need some
+#extra practice project on linked lists because I am not getting this
+
 
 class LinkedListNode #linked node
 
@@ -16,24 +19,6 @@ end
 
 #build stack
 
-class Stack
-
-  attr_reader :data
-
-  def initialize
-    @data = nil
-  end
-
-  def push(newvalue)
-    @data = LinkedListNode.new(newvalue, @data)
-  end
-
-  def pop
-    return print "nil/n" if @data.nil?
-    print "#{@data.value}\n"
-    @data = @data.next_node
-  end
-end
 
 def print_values(list)
   if list
@@ -54,17 +39,33 @@ def reverse_list(list, previous=nil)
   end
 end
 
+def infinite_loop?(list)
+
+  tortoise = list.next_node
+  hare = list.next_node
+
+  until hare.nil?
+    hare = hare.next_node
+    return false if hare.nil?
+
+    hare = hare.next_node
+    tortoise = tortoise.next_node
+    return true if hare == tortoise
+  end
+  return false
+
+end
+
+
 # Main Program
 
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
 
-print_values(node3)
-puts "--------------"
-reverse_list(node3)
-print_values(node1)
-
+puts infinite_loop? (node3)
+node1.next_node = node3
+puts infinite_loop?(node3)
 
 
 
