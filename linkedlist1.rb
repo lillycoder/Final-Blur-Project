beb26d0d0072fb769cfb0b3d4daf1caba558f96e
lillycoder <ltheinh@gmail.com>
2019-08-17 21:54:08 +0000

    finalize Linked List #1 ro TA review

--- linkedlist1.rb
+++ linkedlist1.rb
@@ -1,89 +1,74 @@
 #linked list1 exercise
 #Anne von Luhrte
 #GW Bootcamp
+#with great help from website:  addstar34.github.io/ruby-linked-list-reverse-using-a stack/
+#did not use this website until after I struggled for HOURS and HOURS on this project
 
 class LinkedListNode #linked node
+
   attr_accessor :value, :next_node
 
   def initialize(value, next_node=nil)
     @value = value
     @next_node = next_node
   end
-
-  def reverse_list 
-  end
 end
 
 #build stack
 
-class Stack #linked list
+class Stack
+
+  attr_reader :data
 
   def initialize
-    @nodebottom = LinkedListNode.new(37)
-    @nodemiddle = LinkedListNode.new(99, @nodebottom)
-    @nodetop = LinkedListNode.new(12, @nodemiddle)
+    @data = nil
   end
 
-  def reverse list
-    
+  def push(newvalue)
+    @data = LinkedListNode.new(newvalue, @data)
   end
 
-  def output_list_forward_and_backwards
-    puts "---------------"
-    puts "this is the original list"
-    print_values(@nodetop)
-    puts "this is the list printed backwards"
-    prints_list_backwards(@nodetop)
-    puts "nil"
-    puts "---------------"
-
+  def pop
+    return print "nil/n" if @data.nil?
+    print "#{@data.value}\n"
+    @data = @data.next_node
   end
+end
 
-#print list
-
-  def print_values(list_node)
-    
-    if list_node
-      print "#{list_node.value} --> "
-      print_values(list_node.next_node)
-    else
-      print "nil\n"
-      return
-    end
+def print_values(list)
+  if list
+    print "#{list.value} -->"
+    print_values(list.next_node)
+  else
+    print "nil/n"
+    puts " "
+    return
   end
-
-# prints list backwards
-
-  def prints_list_backwards(list_node)
-    
-    if list_node.next_node == nil
-      print "#{list_node.value} --> "
-    elsif list_node.next_node !=nil
-      prints_list_backwards(list_node.next_node)
-      print "#{list_node.value} --> "
-    end
+end
+   
+def reverse_list(list) 
+  stack = Stack.new
+  while list
+    stack.push(list.value)
+    list  = list.next_node
   end
-
-  def build_new_list(list_node)
-    while list_node.next == nil
-      @reverse_stack_top = LinkedListNode.new(list_node.value)
-    elsif list_node.next_node !=nil
-      @reverse_stack_next = LinkedListNode.new(list_node.value, list_node.next_node)
-      build_new_list(list_node.next_node)
-    end
-  end
-
-  def print_reversed_list
-    
-
+  return stack.data
 end
 
 # Main Program
 
-stack = Stack.new
-stack.output_list_forward_and_backwards
-puts " "
-puts "actual new stack built"
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+puts "--------------"
+revlist = reverse_list(node3)
+print_values(revlist)
+
+
+
+
 
 
 
